224473add12e6dc95b901bce9fecd121cf68c488
Akzhol Asanov <asanov.akzhol04@gmail.com>
2023-06-19 15:23:40 +0600

    123

--- src/components/Search/Pagination.jsx
+++ src/components/Search/Pagination.jsx
@@ -1,2 +1,24 @@
+import React from "react";
+import "./pagination.css";
 
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const pageNumbers = [];
 
+  // Генерация номеров страниц и создание кнопок для каждой страницы
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(
+      // Создание кнопки для каждой страницы
+      <button
+        key={i} // Уникальный ключ элемента в списке (в данном случае, номер страницы)
+        className={currentPage === i ? "active" : ""} // Добавление класса "active" для текущей страницы
+        onClick={() => onPageChange(i)} // Обработчик клика на кнопке, вызывает функцию onPageChange с номером страницы в качестве аргумента
+      >
+        {i}
+      </button>
+    );
+  }
+
+  return <div className="pagination">{pageNumbers}</div>;
+};
+
+export default Pagination;
